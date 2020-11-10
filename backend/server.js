951893ac63d9516a9b66e5f37d32ddc01247552c
Kenneth <kgranados@peoplescout.com>
2020-11-09 21:30:29 -0600

    environment settings

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-    origin: "http://localhost:8081"
+    origin: "http://tombolacr.s3-website-us-east-1.amazonaws.com/"
 };
 
 app.use(cors(corsOptions));
--- frontend/src/app/services/card.service.ts
+++ frontend/src/app/services/card.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-const baseUrl = 'http://localhost:8080/api/cards';
+const baseUrl = 'http://ec2-3-82-60-134.compute-1.amazonaws.com:8080/api/cards';
 
 @Injectable({
   providedIn: 'root'
