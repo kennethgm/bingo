00927d18da4570184a7c5217649bf4d8b8299719
Kenneth <kgranados@peoplescout.com>
2020-11-12 00:44:35 -0600

    Updated s3

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-    origin: "http://latombolacr.com.s3-website-us-east-1.amazonaws.com"
+    origin: "http://latombolacr.com"
 };
 
 app.use(cors(corsOptions));
