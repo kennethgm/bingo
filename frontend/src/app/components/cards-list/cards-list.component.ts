c54e890b5e1cb4d4e9be0751d56dd24507dba4cf
Kenneth <kgranados@peoplescout.com>
2020-12-02 01:15:15 -0600

    Send multiple emails, initial logic.

--- frontend/src/app/components/cards-list/cards-list.component.ts
+++ frontend/src/app/components/cards-list/cards-list.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { GameService } from 'src/app/services/game.service';
 import { CardService } from 'src/app/services/card.service';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-cards-list',
@@ -14,6 +15,12 @@ export class CardsListComponent implements OnInit {
   currentCard = null;
   currentIndex = -1;
   name = '';
+  successMessage = '';
+  currentLink = '';
+  currentDate = '';
+  openSubpage = false;
+  cardsToSend = [];
+  currentEmailId = 0;
 
   constructor(private cardService: CardService, private gameService: GameService) { }
 
@@ -83,7 +90,6 @@ export class CardsListComponent implements OnInit {
   }
 
   openSublist(id) {
-    console.log('called');
     let element = document.getElementById("game-" + id);
     let icon_element = document.getElementById('icon-' + id);
     //console.log('element', element);
@@ -96,5 +102,72 @@ export class CardsListComponent implements OnInit {
       icon_element.setAttribute('class', 'open-submenu fa fa-caret-down');
     }
   }
+
+  openPage(gameCode) {
+    this.openSubpage = true;
+    this.cardsToSend = [];
+    this.currentEmailId = gameCode;
+    this.cards.forEach(element => {
+      if (element.gameCode == gameCode) {
+        this.cardsToSend.push(element);
+      }
+    });
+  }
+
+  sendEmailAll(gameCode) {
+    if(confirm('Esta seguro de que quiere enviar el correo a TODOS los participantes?')) {
+      let index = 0;
+      let self = this;
+      this.cardsToSend.forEach(element => {
+        if (index <= 10) {
+          self.sendByEmail(element.id, gameCode, element);
+        } else {
+          /** TO DO THIS PART OF WAIT */
+          setTimeout(function(){
+            index = 0;
+          }, 2000);
+        }
+        index++;
+      });
+    }
+  }
+
+  sendByEmail(id, gameCode, card){
+    this.successMessage = '';
+    let self = this;
+    let item = document.getElementById("card-"+id+"-"+gameCode);
+    let canvasElement = document.getElementById("canvas-"+id+"-"+gameCode);
+    this.games.forEach(element => {
+      if (element.id == gameCode) {
+        self.currentLink = element.zoomLink;
+        self.currentDate = element.eventDate;
+      }
+    });
+    html2canvas(item, {scrollY: -window.scrollY + 3, width:534, height: 558}).then(canvas => {
+      canvasElement.setAttribute('src',canvas.toDataURL());  
+
+      let requestData = new Object();
+      requestData['emailTo'] =   card.email;
+      requestData['message'] = 'Saludos, \n\n Estimado(a) '+ card.name + '\n\n De parte de Kerberos Producciones'+
+      ' y su marca LaTómbolaCR es un placer servirle en este juego de tómbola virtual. '+
+      ' \n Le deseamos la mejor de las suertes en el juego a realizarse el '+ this.currentDate +' \n '+
+      'Adjuntamos la imágen de su cartón, con un código único y de uso exclusivo para este juego. \n\n '+
+      'Este es link de acceso a la transimión: ' + this.currentLink + '\n\n'+
+      'Se despide atentamente LaTómbolaCR, con una producción más de Kerberos Producciones. \nEsta es nuestra pagina de Facebook, SIGUENOS: https://www.facebook.com/keroproductions/ \n\n' +
+      'Michael Martínez Castro. \n'+
+      'Productor de Kerberos Producciones. \n'+
+      'Contacto: 6161 2298 ';
+      
+      requestData['path'] = canvasElement.getAttribute('src'); 
+      requestData['subject'] = 'Cartón Oficial de La Tómbola CR';
+      
+      this.cardService.sendEmail(requestData).subscribe((data) => {
+        this.successMessage = 'El cartón fue enviado correctamente.';
+      });
+      
+    
+    });
+  }
+
 
 }
